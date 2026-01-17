4615952807aa2d5c898e7a75cd778d9654cb3284
prabhguron <prabhten@gmail.com>
2026-01-17 15:42:15 -0800

    env ooooohhhhh

---
+++ loginSign.js
@@ -0,0 +1,19 @@
+document.getElementById('signInBtn').addEventListener('click', function() {
+    console.log('Sign In clicked');
+    // window.location.href = '/login';
+});
+
+document.getElementById('getStartedBtn').addEventListener('click', function() {
+    console.log('Get Started clicked');
+    // window.location.href = '/signup';
+}); 
+
+document.getElementById('startOrderingBtn').addEventListener('click', function() {
+    console.log('Start Ordering clicked');
+    // window.location.href = '/signup';
+});
+
+document.getElementById('viewMenuBtn').addEventListener('click', function() {
+    console.log('View Menu clicked');
+    // window.location.href = '/login';
+});
